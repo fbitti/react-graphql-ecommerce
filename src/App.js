e77b8f7357fdfd498701f78f344007be49bcde3f
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2023-04-12 13:44:53 +0200

    new GraphQL query to retrieve results by category

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Product from './components/Product';
 import './App.css';
 
 const GET_PRODUCTS = gql`
-  query GetProducts {
-    coffees {
+  query GetProducts($category: String) {
+    coffees(category: $category) {
       id
       title
       description
@@ -18,31 +18,10 @@ const GET_PRODUCTS = gql`
 `;
 
 function App() {
-  const { loading, error, data } = useQuery(GET_PRODUCTS);
-  const [selectedCategory, setSelectedCategory] = useState('All');
-  const [searchQuery, setSearchQuery] = useState('');
-
-  const filterProducts = (products) => {
-    let filteredProducts = products;
-  
-    if (selectedCategory !== 'All') {
-      filteredProducts = filteredProducts.filter(
-        (product) => product.category.toLowerCase() === selectedCategory.toLowerCase()
-      );
-    }
-  
-    if (searchQuery) {
-      const searchRegex = new RegExp(searchQuery, 'i');
-      filteredProducts = filteredProducts.filter(
-        (product) =>
-          searchRegex.test(product.title) ||
-          searchRegex.test(product.description) ||
-          product.ingredients.some((ingredient) => searchRegex.test(ingredient))
-      );
-    }
-  
-    return filteredProducts;
-  };
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const { loading, error, data } = useQuery(GET_PRODUCTS, {
+    variables: { category: selectedCategory },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) {
@@ -54,23 +33,17 @@ function App() {
     <div className="App">
       <h1>Product Catalog</h1>
       <div className="filters">
-        <button onClick={() => setSelectedCategory('All')}>All</button>
+        <button onClick={() => setSelectedCategory(null)}>All</button>
         <button onClick={() => setSelectedCategory('Hot')}>Hot</button>
         <button onClick={() => setSelectedCategory('Iced')}>Iced</button>
-        <input
-          type="text"
-          placeholder="Search..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
       </div>
       <div className="products">
-        {filterProducts(data.coffees).map((product) => (
+        {data && data.coffees.map((product) => (
           <Product key={product.id} product={product} />
         ))}
       </div>
     </div>
-  );
+  );  
 }
 
 export default App;
