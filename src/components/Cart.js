f322ac0cf0043635421751b646fcf21df477aa31
Fernando Bitti Loureiro <fernando.bitti@gmail.com>
2023-12-12 16:23:19 +0100

    quantity update buttons in the shopping cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, useMutation, gql } from '@apollo/client';
 
 const GET_CART = gql`
   query GetCart($userId: ID!) {
@@ -12,10 +12,42 @@ const GET_CART = gql`
   }
 `;
 
+const UPDATE_CART_ITEM = gql`
+  mutation UpdateCartItem($userId: ID!, $productId: ID!, $quantity: Int!) {
+    updateCartItem(userId: $userId, productId: $productId, quantity: $quantity) {
+      id
+      quantity
+    }
+  }
+`;
+
+const REMOVE_CART_ITEM = gql`
+  mutation RemoveCartItem($userId: ID!, $productId: ID!) {
+    removeCartItem(userId: $userId, productId: $productId)
+  }
+`;
+
 function Cart({ userId, userName }) {
-  const { loading, error, data } = useQuery(GET_CART, {
+  const { loading, error, data, refetch } = useQuery(GET_CART, {
     variables: { userId },
   });
+
+  // Define useMutation hooks
+  const [updateCartItem] = useMutation(UPDATE_CART_ITEM);
+  const [removeCartItem] = useMutation(REMOVE_CART_ITEM);
+
+
+  // Handle increment and decrement
+  const changeItemQuantity = (productId, quantity) => {
+    updateCartItem({ variables: { userId, productId, quantity } })
+      .then(() => refetch());
+  };
+
+  // Handle item removal
+  const handleRemoveItem = (productId) => {
+    removeCartItem({ variables: { userId, productId } })
+      .then(() => refetch());
+  };
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
@@ -32,7 +64,10 @@ function Cart({ userId, userName }) {
       <ul>
         {cartItems.map((item) => (
           <li key={item.id}>
-            {item.title} - ${item.price.toFixed(2)} x {item.quantity} = ${(item.price * item.quantity).toFixed(2)}
+            {item.title} - ${item.price} x {item.quantity} = ${(item.price * item.quantity).toFixed(2)}
+            <button onClick={() => changeItemQuantity(item.id, item.quantity + 1)}>+1</button>
+            <button onClick={() => changeItemQuantity(item.id, item.quantity - 1)}>-1</button>
+            <button onClick={() => handleRemoveItem(item.id)}>Remove</button>
           </li>
         ))}
       </ul>
